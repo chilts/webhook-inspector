eb3c1df1a3dba69f08966051d1858a2381a89f68
Glitch (hello-express) <none>
2019-07-10 11:16:03 +0000

    🗻🚔 Checkpoint ./package.json:5467840/1210 ./views/index.html:5467840/311 ./views/index.pug:5467840/11077 ./server.js:5467840/424

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
-// server.js
-// where your node app starts
+const http = require('http')
 
-// init project
-const express = require('express');
-const app = express();
+const express = require('express')
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+const app = express()
+app.set('view-engine', 'pug')
 
-// http://expressjs.com/en/starter/static-files.html
+
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
+app.get('/', function(req, res) {
+  res.render('index')
+})
 
 // listen for requests :)
+const server = http
+
 const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
