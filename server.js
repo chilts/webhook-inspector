6a556b7774a5183b49293270de0c5b98a4e3ff88
Andrew Chilton <chilts@webdev.sh>
2019-07-10 21:53:32 +0000

    ☕️🐉 Checkpoint ./server.js:5467840/404 ./package.json:5467840/830

--- server.js
+++ server.js
@@ -4,23 +4,24 @@ const crypto = require('crypto')
 
 // npm
 const express = require('express')
+const bodyParser = require('body-parser')
+const cookieSession = require('cookie-session')
 const level = require('level')
 
 // setup
 const githubAppSecret = process.env.GITHUB_APP_SECRET
+const sessionKey = process.env.SESSION_KEY
 const db = level('.data/store.db')
 
 function checkGitHubSignature(req, res, next) {
   
   const hmac = crypto.createHmac('sha1', githubAppSecret)
 
-  hmac.update(req.body);
-  console.log(hmac.digest('hex'));
-// Prints:
-//   7fd04df92f636fd450bc841c9418e5825c17f33ad9c87c518115a45971f7f77e
+  hmac.update(req.body)
+  console.log(hmac.digest('hex'))
+  // Prints:
+  //   7fd04df92f636fd450bc841c9418e5825c17f33ad9c87c518115a45971f7f77e
 
-  
-  
   next()
 }
 
@@ -33,8 +34,12 @@ app.set('view engine', 'pug')
 app.enable('trust proxy')
 
 app.use(express.static('public'))
-
 app.use(bodyParser.raw())
+app.use(cookieSession({
+  name   : 'session',
+  keys   : [ sessionKey ],
+  maxAge : ,
+}))
 
 app.get('/', (req, res) => {
   res.render('index')
